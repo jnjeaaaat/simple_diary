371f3e7a390a81d604088e428ba1f115bbbf0ac3
parktj <ekswn120@gmail.com>
2022-10-09 15:26:01 +0900

    Initial commit

---
+++ src/main/java/com/example/simple_diary/SimpleDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simple_diary/SimpleDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
